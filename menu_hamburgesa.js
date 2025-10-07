0537b20127faf3841ea543690c9217ea329c890f
MODULAR DESIGN <psolaz@campuscamarafp.com>
2025-10-07 12:56:09 +0200

    vinilo puesto con precio y descripcion

--- menu_hamburgesa.js
+++ menu_hamburgesa.js
@@ -2,12 +2,11 @@ const btn = document.getElementById("hamburger-btn");
 const menu = document.getElementById("menu-lateral");
 
 btn.addEventListener("click", () => {
-    menu.classList.toggle("open");
+  menu.classList.toggle("open");
 });
 
-// Cierra el menú al hacer clic fuera de él
 menu.addEventListener("click", (e) => {
-    if (e.target === menu) {
-        menu.classList.remove("open");
-    }
+  if (e.target === menu) {
+    menu.classList.remove("open");
+  }
 });
