97407558906a840d6741706a22d8a4cd4404c614
MODULAR DESIGN <psolaz@campuscamarafp.com>
2025-12-16 12:25:25 +0100

    añadida la pagina catalogo y los headers donde tocan

---
+++ catalogo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Catálogo</title>
+</head>
+<body>
+    <header class="header-inicio">
+    <div class="logo">
+      <a href="index.html">
+        <img src="images/logo.png" alt="Logo de la empresa">
+      </a>
+    </div>
+    
+    <button class="hamburger-btn" id="hamburger-btn">☰</button>
+
+    <nav class="menu-lateral" id="menu-lateral">
+      <ul>
+        <li><a href="#logo">Inicio</a></li>
+        <li><a href="catalogo.php">Catálogo</a></li>
+        <li><a href="nosotros.html">Nosotros</a></li>
+        <li><a  href="#contacto">Contacto</a></li>
+        <a href="login.php" class="boton-descubre">Gestión catálogo</a>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
